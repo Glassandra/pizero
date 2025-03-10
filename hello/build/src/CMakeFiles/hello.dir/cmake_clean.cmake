27e4f5aad1cda884d20974a4d8f94b8403c3e4f7
Glassandra <cassandra.larsson.se@gmail.com>
2025-03-10 13:30:48 +0100

    added hello world

---
+++ hello/build/src/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/cass/Documents/Projects/gittest2/pizero/hello/bin/hello"
+  "/home/cass/Documents/Projects/gittest2/pizero/hello/bin/hello.pdb"
+  "CMakeFiles/hello.dir/main.cpp.o"
+  "CMakeFiles/hello.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
